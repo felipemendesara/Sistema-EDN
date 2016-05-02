42885e18fcec515415e8c7083a7ddc5a0bf4c563
Felipe Mendes <felipemendesara123@gmail.com>
2016-05-02 09:04:02 -0300

    s

--- Models/Eventos.cs
+++ Models/Eventos.cs
@@ -29,7 +29,7 @@ namespace EDNEVENTOS.Models
         public int CepEvento { get; set; }
         [Required]
         [Display(Name = "Imagem")]
-        public byte[] ImagemProduto { get; set; }
+        public string ImagemProduto { get; set; }
         [Required]
         [Display(Name = "Status do Evento")]
         public bool Status { get; set; }
